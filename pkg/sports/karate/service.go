728b7978756482a153a41908d2a561e6c1c544b2
Geniuskaa <eminizinar88@gmail.com>
2022-08-14 17:36:11 +0300

    Some improvements of parser response and letter response feature added

--- pkg/sports/karate/service.go
+++ pkg/sports/karate/service.go
@@ -9,14 +9,12 @@ import (
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/lib/pq"
-	"log"
-	"time"
 )
 
 type Service struct {
 	db         *database.Postgres
 	ctx        context.Context
-	categories map[string]map[pgtype.Int4range]map[string]*catIdLeave
+	categories map[string]map[pgtype.Int4range]map[string]*catIdLeaf
 }
 
 type competitionCategory struct {
@@ -29,9 +27,16 @@ type competitionCategory struct {
 	groupKata  bool
 }
 
-type catIdLeave struct {
+type catIdLeaf struct {
 	categoryId int
 	weightMap  map[pgtype.Int4range]int
+}
+
+type Response struct {
+	CountOfFailedRows int
+	ErrsOfFailedRows  []error
+	AddedParticipants []string
+	CountOfAddedParts int
 }
 
 func NewService(db *database.Postgres, ctx context.Context) *Service {
@@ -44,7 +49,7 @@ func NewService(db *database.Postgres, ctx context.Context) *Service {
 	return &Service{db: db, ctx: ctx, categories: m}
 }
 
-func categoryMapInitializer(pool *pgxpool.Pool, ctx context.Context) (map[string]map[pgtype.Int4range]map[string]*catIdLeave, error) {
+func categoryMapInitializer(pool *pgxpool.Pool, ctx context.Context) (map[string]map[pgtype.Int4range]map[string]*catIdLeaf, error) {
 	rows, err := pool.Query(ctx, `select id, kata_or_kumite, sex, age, kyi, weight, group_kata from karate_category;`)
 	if err != nil {
 		return nil, fmt.Errorf("categoryMapInitializer failed: %w", err)
@@ -52,50 +57,49 @@ func categoryMapInitializer(pool *pgxpool.Pool, ctx context.Context) (map[string
 
 	defer rows.Close()
 
-	m := make(map[string]map[pgtype.Int4range]map[string]*catIdLeave, 2)
-	m["кат"] = make(map[pgtype.Int4range]map[string]*catIdLeave, 6)
-	m["кум"] = make(map[pgtype.Int4range]map[string]*catIdLeave, 6)
+	m := make(map[string]map[pgtype.Int4range]map[string]*catIdLeaf, 2)
+	m["кат"] = make(map[pgtype.Int4range]map[string]*catIdLeaf, 6)
+	m["кум"] = make(map[pgtype.Int4range]map[string]*catIdLeaf, 6)
 
 	for rows.Next() {
 		compCat := &competitionCategory{}
 
 		err := rows.Scan(&compCat.id, &compCat.kataKumite, &compCat.sex, &compCat.age, &compCat.kyi, &compCat.weight, &compCat.groupKata)
 		if err != nil {
-
+			return nil, fmt.Errorf("categoryMapInitializer failed: %w", err)
 		}
 
 		switch compCat.kataKumite {
 		case "кат":
 			v, ok := m["кат"][compCat.age]
 			if ok == false {
-				m["кат"][compCat.age] = make(map[string]*catIdLeave, 3)
-				m["кат"][compCat.age][compCat.sex] = &catIdLeave{categoryId: compCat.id}
-				continue
+				m["кат"][compCat.age] = make(map[string]*catIdLeaf, 3)
+				m["кат"][compCat.age][compCat.sex] = &catIdLeaf{categoryId: compCat.id}
+			} else {
+				v[compCat.sex] = &catIdLeaf{categoryId: compCat.id}
 			}
-			v[compCat.sex] = &catIdLeave{categoryId: compCat.id}
 		case "кум":
 			v, ok := m["кум"][compCat.age]
 			if ok == false {
-				m["кум"][compCat.age] = make(map[string]*catIdLeave, 2)
+				m["кум"][compCat.age] = make(map[string]*catIdLeaf, 2)
 
-				leave, exists := m["кум"][compCat.age][compCat.sex]
+				_, exists := m["кум"][compCat.age][compCat.sex]
 				if exists == false {
-					m["кум"][compCat.age][compCat.sex] = &catIdLeave{weightMap: make(map[pgtype.Int4range]int, 6)}
+					m["кум"][compCat.age][compCat.sex] = &catIdLeaf{weightMap: make(map[pgtype.Int4range]int, 6)}
 					m["кум"][compCat.age][compCat.sex].weightMap[compCat.weight] = compCat.id
-					continue
 				}
-				leave.weightMap[compCat.weight] = compCat.id
+				// Скорее всего это уже не нужно, но если что пусть первые 3 продакшен версии будет тут
+				//leave.weightMap[compCat.weight] = compCat.id
 
-				continue
+			} else {
+				leave, exists := v[compCat.sex]
+				if exists == false {
+					v[compCat.sex] = &catIdLeaf{weightMap: make(map[pgtype.Int4range]int, 6)}
+					v[compCat.sex].weightMap[compCat.weight] = compCat.id
+				} else {
+					leave.weightMap[compCat.weight] = compCat.id
+				}
 			}
-
-			leave, exists := v[compCat.sex]
-			if exists == false {
-				v[compCat.sex] = &catIdLeave{weightMap: make(map[pgtype.Int4range]int, 6)}
-				v[compCat.sex].weightMap[compCat.weight] = compCat.id
-				continue
-			}
-			leave.weightMap[compCat.weight] = compCat.id
 		}
 
 	}
@@ -103,31 +107,32 @@ func categoryMapInitializer(pool *pgxpool.Pool, ctx context.Context) (map[string
 	return m, nil
 }
 
-func (s *Service) UploadParticipants(m map[string]interface{}, uuid string) error {
+func (s *Service) UploadParticipants(m map[string]interface{}, uuid string) (*Response, error) {
 
 	temp := s.db.Pool.QueryRow(s.ctx, `SELECT id from competition where uuid = $1`, uuid)
 	var competId int64
 	err := temp.Scan(&competId)
 	if err != nil {
-		return fmt.Errorf("Competition id cast to int64 failed", err)
+		return nil, fmt.Errorf("Competition id cast to int64 failed: %w", err)
 	}
 
-	batch := &pgx.Batch{}
+	resp := Response{CountOfFailedRows: 0, ErrsOfFailedRows: make([]error, 0, len(m)), AddedParticipants: make([]string, 0, len(m)), CountOfAddedParts: 0}
+	rows := make([]pgx.Row, 0, len(m))
 
-	start := time.Now()
 	for _, v := range m {
 		p := v.(Participant)
 
 		ids := make([]int, 0, 3)
 
+		// Всё, что тут происходит - не логгируется, в конце мы лишь запишем имена тех, кого успешно добавили и напишем
+		// сколько человек было с ошибкой.
 		if p.KataKumite[0] {
 			if p.KataKumite[1] && p.KataGroup { // ката + ката группа + кумите
 				// categories[] = {kata: true/false, kataGroup: true/false, kumite: true/false}
 				err = s.getCategoryIds(&ids, &p, [3]bool{true, true, true})
 				if err != nil {
-					log.Println(err)
-
-					//TODO: обработать ошибку
+					resp.ErrsOfFailedRows = append(resp.ErrsOfFailedRows, err)
+					resp.CountOfFailedRows++
 					continue
 				}
 
@@ -135,27 +140,24 @@ func (s *Service) UploadParticipants(m map[string]interface{}, uuid string) erro
 				// categories[] = {kata: true/false, kataGroup: true/false, kumite: true/false}
 				err = s.getCategoryIds(&ids, &p, [3]bool{true, true, false})
 				if err != nil {
-					log.Println(err)
-
-					//TODO: обработать ошибку
+					resp.ErrsOfFailedRows = append(resp.ErrsOfFailedRows, err)
+					resp.CountOfFailedRows++
 					continue
 				}
 			} else if p.KataKumite[1] { // ката + кумите
 				// categories[] = {kata: true/false, kataGroup: true/false, kumite: true/false}
 				err = s.getCategoryIds(&ids, &p, [3]bool{true, false, true})
 				if err != nil {
-					log.Println(err)
-
-					//TODO: обработать ошибку
+					resp.ErrsOfFailedRows = append(resp.ErrsOfFailedRows, err)
+					resp.CountOfFailedRows++
 					continue
 				}
-			} else {
+			} else { // только ката
 				// categories[] = {kata: true/false, kataGroup: true/false, kumite: true/false}
 				err = s.getCategoryIds(&ids, &p, [3]bool{true, false, false})
 				if err != nil {
-					log.Println(err)
-
-					//TODO: обработать ошибку
+					resp.ErrsOfFailedRows = append(resp.ErrsOfFailedRows, err)
+					resp.CountOfFailedRows++
 					continue
 				}
 			}
@@ -163,43 +165,37 @@ func (s *Service) UploadParticipants(m map[string]interface{}, uuid string) erro
 			// categories[] = {kata: true/false, kataGroup: true/false, kumite: true/false}
 			err = s.getCategoryIds(&ids, &p, [3]bool{false, false, true})
 			if err != nil {
-				log.Println(err)
-				//TODO: обработать ошибку
+				resp.ErrsOfFailedRows = append(resp.ErrsOfFailedRows, err)
+				resp.CountOfFailedRows++
 				continue
 			}
 		}
 
-		batch.Queue(`insert into karate_participant (fullname, age, weight, kyi, dan, city, coach_fullname, 
+		row := s.db.Pool.QueryRow(s.ctx, `insert into karate_participant (fullname, age, weight, kyi, dan, city, coach_fullname, 
 					competition_id, karate_category_ids) values ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning karate_participant.fullname;`,
 			p.FullName, p.Age, p.Weight, p.Kyi, p.Dan, p.City, p.Coach, competId, pq.Array(ids))
+		rows = append(rows, row)
 	}
 
-	batchRes := s.db.Pool.SendBatch(s.ctx, batch)
-	rows, err := batchRes.Query() // .Exec()
-	if err != nil {
-		return err
-	}
-	fmt.Println("Функция отработала за: ", time.Since(start).Milliseconds(), " ms")
+	for _, row := range rows {
+		var uploadedPart string
 
-	countOfSucces := 0
-	fmt.Println("Информация по добавленным юзерам")
-	for rows.Next() {
-		var uploadedPartt string
-
-		err := rows.Scan(&uploadedPartt)
+		err := row.Scan(&uploadedPart)
 		if err != nil {
-			log.Println(err)
+			resp.ErrsOfFailedRows = append(resp.ErrsOfFailedRows, err)
 			continue
 		}
 
-		countOfSucces++
-		fmt.Println(fmt.Sprintf("User: %s был добавлен в систему", uploadedPartt))
+		resp.AddedParticipants = append(resp.AddedParticipants, uploadedPart)
 	}
 
-	fmt.Println("Всего добавлено ", countOfSucces, " участников")
+	resp.CountOfAddedParts = len(resp.AddedParticipants)
 
-	fmt.Println("All participants succesfully were added to DB!")
-	return nil
+	if resp.CountOfFailedRows < (len(m) - resp.CountOfAddedParts) {
+		resp.CountOfFailedRows = len(m) - resp.CountOfAddedParts
+	}
+
+	return &resp, nil
 }
 
 func (s *Service) idFinderKata(p *Participant) (int, error) {
@@ -310,7 +306,5 @@ func (s *Service) getCategoryIds(ids *[]int, p *Participant, categories [3]bool)
 		*ids = append(*ids, id)
 	}
 
-	fmt.Println("Дошли до конца")
-	fmt.Println(*ids)
 	return nil
 }
